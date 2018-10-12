9ba424fab898ab8c0bf9a97ad788c7c90c921f35
zhaojianxing <18716368481@163.com>
2018-10-12 23:01:58 +0800

    添加注释

--- test.py
+++ test.py
@@ -2,9 +2,12 @@
 person_message = {"name":'zhaojianxing',"age":26,"height":"171cm","weight":"62.5kg"}
 
 school_message = {"s_name":'华北科技学院','s_address':'燕郊'}
-
+"""
+这是一种合并字典的方法，很是方便简洁
+"""
 
 dict1 = dict(person_message,**school_message)
 
 print(dict1)
+ 
 
